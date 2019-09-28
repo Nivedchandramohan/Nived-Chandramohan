fbae507cca49136c0a3395850591ed79d9a613d1
nived <tresrappers2@gmail.com>
2019-09-28 14:02:09 -0400

    class 9-28

--- Python Homework.py
+++ Python Homework.py
@@ -1,8 +1,9 @@
-num = int(input("Enter a number: "))
-if (num % 2) == 0:
-   print("{0} is Even number".format(num))
+a = int(input("Enter first number"))
+b = int(input("Enter second number"))
+if(a>b):
+    print("First number is greater than second number")
 else:
-   print("{0} is Odd number".format(num))
+    print("First number is not greater than second number")
 
 
 
